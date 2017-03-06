ef0ae5238c5fb35cb40720435f7386f05eae2e80
Nina Ma <ninam88@gmail.com>
2017-03-06 17:17:44 +0100

    testing calendar with some fake events

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,32 +1,53 @@
 import React, {Component} from 'react';
 import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import exercises from '../data/exercises';
+
 import moment from 'moment';
 // Setup the localizer by providing the moment Object
 
+function getSpecificExercise ( obj ) {
+  return obj.muscle === 'shoulders';
+}
+var test = exercises.filter(getSpecificExercise);
 
-const myEventsList =[];
+
+var hej = test.map(function(el){
+  return el.name;
+});
+
+// console.log(test);
+// console.log(hej);
+
+const myEventsList =[{
+          'title': 'Leg',
+          'allDay': true,
+          'start': new Date("Feb 23 2017"),
+          'end': new Date("Feb 23 2017")
+        },
+        {
+          'title': 'Arm',
+          'allDay': false,
+          'start': new Date("Feb 23 2017"),
+          'end': new Date("Feb 23 2017")
+        }];
 
 class MyCalendar extends Component{
   constructor(){
     super()
     BigCalendar.momentLocalizer(moment);
-    this.openEventModal = this.openEventModal.bind(this);
-  }
 
-  openEventModal(){
-    this.props.openModal();
   }
 
   render(){
+    const props = this.props;
     return(
-      <div className="c-calender">
+      <div className="c-calendar">
         <BigCalendar
         events={myEventsList}
-        startAccessor='startDate'
-        endAccessor='endDate'
         views={['month']}
         selectable={true}
-        onSelectSlot={this.openEventModal}
+        onSelectSlot={props.openModal}
         />
       </div>
     );
