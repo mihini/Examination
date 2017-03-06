1bb3f8044e39f4333c909904ee3dce0c8434314a
Nina Ma <ninam88@gmail.com>
2017-03-06 17:15:18 +0100

    added exercise data

---
+++ src/data/exercises.js
@@ -0,0 +1,24 @@
+const exercises = [
+  {
+    name:"ab roller",
+    muscle:"abdominals"
+  },
+  {name:"adductor", muscle:"adductors"},
+  {name:"all fours quad stretch", muscle:"quadriceps"},
+  {name:"alternate hammer curl", muscle:"biceps"},
+  {name:"alternate heel touchers", muscle:"abdominals"},
+  {name:"alternate incline dumbbell curl", muscle:"biceps"},
+  {name:"alternating cable shoulder press", muscle:"shoulders"},
+  {name:"alternating deltoid raise", muscle:"shoulders"},
+  {name:"alternating floor press", muscle:"chest"},
+  {name:"alternating hang clean", muscle:"hamstrings"},
+  {name:"tate press", muscle:"triceps"},
+  {name:"the straddle", muscle:"hamstrings"},
+  {name:"thigh abductor", muscle:"abductors"},
+  {name:"thigh adductor", muscle:"adductors"},
+  {name:"tire flip", muscle:"quadriceps"},
+  {name:"toe touchers", muscle:"abdominals"},
+
+
+]
+export default exercises;
